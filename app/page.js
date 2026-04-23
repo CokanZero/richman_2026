759bec7b25beba733a7511df45b987a64cbd42bf
Yejin Kang <your-email@example.com>
2026-04-23 13:19:33 +1000

    temporary hidden: toggle

--- app/page.js
+++ app/page.js
@@ -21,10 +21,10 @@ const TEXT = {
 }
 
 const TYPES = [
-  { value: '1', label: 'Type A (BE-LM)' },
-  { value: '2', label: 'Type B (BE-DM)' },
-  { value: '3', label: 'Type C (DE-LM)' },
-  { value: '4', label: 'Type D (DE-DM)' },
+  { value: '1', label: 'Type A (Bright-Light Mode)' },
+  { value: '2', label: 'Type B (Bright-Dark Mode)' },
+  { value: '3', label: 'Type C (Dark-Light Mode)' },
+  { value: '4', label: 'Type D (Dark-Dark Mode)' },
 ]
 
 function HomeInner() {
